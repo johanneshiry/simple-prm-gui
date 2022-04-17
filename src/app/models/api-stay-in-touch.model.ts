fe5dd2ee0089749fd5c3da26ceef021f3dee96df
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2022-04-17 23:51:16 +0200

    api stay in touch model

---
+++ src/app/models/api-stay-in-touch.model.ts
@@ -0,0 +1,7 @@
+import {Duration, ZonedDateTime} from "@js-joda/core";
+
+export interface ApiStayInTouch {
+    contactId: string
+    lastContacted: ZonedDateTime
+    contactInterval: Duration
+}
