d70f72bf32333ff7db68d46066d1db9e7c7f5bd5
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2022-04-21 21:41:46 +0200

    added reminder query support in contact detail

---
+++ src/app/services/reminder.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { GlobalConstants } from "../common/global-constants";
+import { Observable } from "rxjs";
+import { ApiReminder } from "../models/api/reminder.model";
+import { Reminder } from "../models/reminder.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ReminderService {
+  constructor(private http: HttpClient) {}
+
+  create(stayInTouch: Reminder) {
+    return this.http.post<Reminder>(
+      GlobalConstants.apiReminderUrl + "/",
+      stayInTouch
+    );
+  }
+
+  get(contactUid: string): Observable<ApiReminder[]> {
+    return this.http.get<ApiReminder[]>(
+      GlobalConstants.apiReminderUrl + `/${contactUid}`
+    );
+  }
+}
--- src/app/views/contacts/contact-detail/contact-detail.component.ts
+++ src/app/views/contacts/contact-detail/contact-detail.component.ts
@@ -2,6 +2,11 @@ import { Component, Input, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Contact } from "../../../models/contact.model";
 import { DummyContactComponent } from "./DummyContact.component";
+import { ReminderService } from "../../../services/reminder.service";
+import { Reminder } from "../../../models/reminder.model";
+import { ApiContact } from "../../../models/api/api-contact.model";
+import { ApiReminder } from "../../../models/api/reminder.model";
+import { ContactService } from "../../../services/contact.service";
 
 @Component({
   selector: "app-contact-detail",
@@ -11,19 +16,44 @@ import { DummyContactComponent } from "./DummyContact.component";
 export class ContactDetailComponent implements OnInit {
   @Input() contact?: Contact = DummyContactComponent.dummyContact();
 
-  _avatarBackgroundColor = "secondary";
+  avatarBackgroundColor = "secondary";
+  reminders?: Reminder[];
 
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private reminderService: ReminderService,
+    private contactService: ContactService
+  ) {
     const state = this.router.getCurrentNavigation()?.extras.state as {
       contact: Contact;
     };
     if (state != undefined) {
+      // todo switch to service to enable page reloading
       this.contact = state.contact;
       this.route.snapshot.data = { title: this.contact.fn };
     } else {
       this.route.snapshot.data = { title: "No contact selected!" };
     }
+
+    // retrieve reminders
+    this.retrieveReminders();
   }
 
   ngOnInit(): void {}
+
+  private retrieveReminders() {
+    if (this.contact?.uid != undefined) {
+      this.reminderService.get(this.contact.uid).subscribe({
+        next: (apiReminder: ApiReminder[]) => {
+          if (apiReminder.length != 0) {
+            this.reminders = apiReminder.map(
+              (apiReminder) => new Reminder(apiReminder)
+            );
+          }
+        },
+        error: (e) => console.error(e),
+      });
+    }
+  }
 }
