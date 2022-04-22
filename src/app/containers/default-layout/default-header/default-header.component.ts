8a24a495dfb86ad827d38b8b9d5c58390bbd4000
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2022-04-22 15:41:46 +0200

    kill sidebar + new dropdown mobile nav

--- src/app/containers/default-layout/default-header/default-header.component.ts
+++ src/app/containers/default-layout/default-header/default-header.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from "@angular/core";
 
 import { ClassToggleService, HeaderComponent } from "@coreui/angular";
+import { navItems } from "../_nav";
 
 @Component({
   selector: "app-default-header",
@@ -9,11 +10,14 @@ import { ClassToggleService, HeaderComponent } from "@coreui/angular";
 export class DefaultHeaderComponent extends HeaderComponent {
   @Input() sidebarId: string = "sidebar";
 
+  public navItems = navItems;
+
   public newMessages = new Array(4);
   public newTasks = new Array(5);
   public newNotifications = new Array(5);
 
   constructor(private classToggler: ClassToggleService) {
     super();
+    console.log(JSON.stringify(navItems));
   }
 }
